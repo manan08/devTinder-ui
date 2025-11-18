61ff2654ae2c944a73868bb980197cb8d7c8b9f4
manan08 <72185965+manan08@users.noreply.github.com>
2025-11-19 02:18:22 +0530

    BrowserRouter - Navbar, Footer, Body - Profile & Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Body from './Body'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World!</h1>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Body />}>
+            <Route path='/login' element={<Login />} />
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
